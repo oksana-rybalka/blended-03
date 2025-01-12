757c3495fba9d6335aed0cd2eb487025f63fb67a
oksana-rybalka <rybalka-oksana@outlook.com>
2025-01-12 11:31:06 +0200

    getCountry

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 import Heading from './components/Heading/Heading';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import Header from './components/Header/Header';
+
+const Home = lazy(() => import('./pages/Home'));
+const SearchCountry = lazy(() => import('./pages/SearchCountry'));
+const Country = lazy(() => import('./pages/Country'));
 
 export const App = () => {
-  return <Heading title="App" bottom />;
+  return (
+    <>
+      <Header />
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country" element={<SearchCountry />} />
+          <Route path="/country/:countryId" element={<Country />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
 import Container from '../components/Container/Container';
 import Heading from '../components/Heading/Heading';
 import Section from '../components/Section/Section';
+import { getCountries } from '../service/countryApi';
 
 const Home = () => {
+  const [country, setCountry] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      setIsLoading(true);
+      try {
+        const newCountries = await getCountries();
+        setCountry(newCountries);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCountries();
+  }, []);
+
+  console.log(country);
   return (
     <Section>
       <Container>
